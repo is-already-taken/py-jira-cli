5c0546a3bbfc48afa16e252a07967ee14caae761
Thomas Lehmann <thomas.lehmann@stueckseln.de>
2015-02-12 23:20:16 +0100

    Added tests for Styled() and CompositeStyled()

--- printer_test.py
+++ printer_test.py
@@ -47,7 +47,108 @@ class PrinterTest(unittest.TestCase):
 		self.assertEqual(s, "EXEED TO EIGHTTEEN" + " PQR " + (" " * 7) + "ABC", "correct output")
 
 
-	#
-	# Styled() is not tested, I havn't figured out how
-	# to mock module import errors
-	#
+class CompositeStyledTest(unittest.TestCase):
+
+	"""It should return the sum length of both strings"""
+	def test_str_concat(self):
+		# using strings.
+		# any class implementing __str__() and __len__() should work
+		comp = printer.CompositeStyled("123", "4567")
+
+		self.assertEqual(str(comp), "1234567", "__str__() should concat A + B")
+
+	"""It should return the concatenated strings """
+	def test_sen_sum(self):
+		# using strings.
+		# any class implementing __str__() and __len__() should work
+		comp = printer.CompositeStyled("123", "4567")
+
+		self.assertEqual(len(comp), 7, "Sum length should be sum of A + B")
+
+
+
+class StyledTest(unittest.TestCase):
+
+	def setUp(self):
+		# changing this during test: save original value
+		self._orig_styled_use_color = printer.Styled.use_color
+		self._orig_styled_fg = printer.Styled.fg
+		self._orig_styled_bg = printer.Styled.bg
+		self._orig_styled_reset = printer.Styled.reset
+
+	def tearDown(self):
+		printer.Styled.use_color = self._orig_styled_use_color
+		printer.Styled.fg = self._orig_styled_fg
+		printer.Styled.bg = self._orig_styled_bg
+		printer.Styled.reset = self._orig_styled_reset
+
+
+	def test_no_color_str_and_len(self):
+		printer.Styled.use_color = False
+
+		styled = printer.Styled("1234")
+
+		self.assertEqual(str(styled), "1234", "__str__() returns raw string")
+		self.assertEqual(len(styled), 4, "len() returns raw string's length")
+
+	def test_no_color_concat_to_str(self):
+		printer.Styled.use_color = False
+
+		styled = printer.Styled("1234")
+
+		concated = styled + "ABC"
+
+		self.assertEqual(str(concated), "1234ABC", "__str__() returns raw string")
+		self.assertEqual(len(concated), 7, "len() returns both string raw length")
+		self.assertEqual(type(concated), printer.CompositeStyled, "Styled() + str() returns CompositeStyled")
+
+	def test_no_color_concat_to_styled(self):
+		printer.Styled.use_color = False
+
+		styled = printer.Styled("1234")
+
+		concated = styled + printer.Styled("9876")
+
+		self.assertEqual(str(concated), "12349876", "__str__() returns raw string")
+		self.assertEqual(len(concated), 8, "len() returns both string raw length")
+		self.assertEqual(type(concated), printer.CompositeStyled, "Styled() + str() returns CompositeStyled")
+
+	def test_using_color_str_and_len(self):
+		printer.Styled.use_color = True
+		printer.Styled.fg = {"red": "[R]"}
+		printer.Styled.bg = {"green": "[G]"}
+		printer.Styled.reset = "[_]"
+
+		styled = printer.Styled("1234", fore=printer.Styled.fg["red"], back=printer.Styled.bg["green"])
+
+		self.assertEqual(str(styled), "[R][G]1234[_]", "__str__() returns raw string")
+		self.assertEqual(len(styled), 4, "len() returns raw string's length")
+
+	def test_using_color_concat_to_str(self):
+		printer.Styled.use_color = True
+		printer.Styled.fg = {"red": "[R]", "yellow": "[Y]"}
+		printer.Styled.bg = {"green": "[G]", "blue": "[B]"}
+		printer.Styled.reset = "[_]"
+
+		styled = printer.Styled("1234", fore=printer.Styled.fg["red"], back=printer.Styled.bg["green"])
+
+		concated = styled + "ABC"
+
+		self.assertEqual(str(concated), "[R][G]1234[_]ABC", "__str__() returns raw string")
+		self.assertEqual(len(concated), 7, "len() returns both string raw length")
+		self.assertEqual(type(concated), printer.CompositeStyled, "Styled() + str() returns CompositeStyled")
+
+	def test_using_color_concat_to_styled(self):
+		printer.Styled.use_color = True
+		printer.Styled.fg = {"red": "[R]", "yellow": "[Y]"}
+		printer.Styled.bg = {"green": "[G]", "blue": "[B]"}
+		printer.Styled.reset = "[_]"
+
+		styled = printer.Styled("1234", fore=printer.Styled.fg["red"], back=printer.Styled.bg["green"])
+
+		concated = styled + printer.Styled("9876", fore=printer.Styled.fg["yellow"], back=printer.Styled.bg["blue"])
+
+		self.assertEqual(str(concated), "[R][G]1234[_][Y][B]9876[_]", "__str__() returns raw string")
+		self.assertEqual(len(concated), 8, "len() returns both string raw length")
+		self.assertEqual(type(concated), printer.CompositeStyled, "Styled() + str() returns CompositeStyled")
+
